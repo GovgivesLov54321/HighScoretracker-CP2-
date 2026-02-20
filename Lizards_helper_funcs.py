c55032073b7b41fe71629f219e56ffe1464287ea
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-02-19 20:13:27 -0700

    Create Lizards_helper_funcs.py with helper functions pseudocode
    Add initial helper functions and documentation.

---
+++ Lizards_helper_funcs.py
@@ -0,0 +1,5 @@
+#Contains useful helper functions for all sorts of applications
+
+#stupid proofed inputs function
+
+#onlinator - when the onlinator decorator is applied to a function such as main, it recreates the while loop that keeps the code running until the user chooses to quit, I may make a second version for main which has a different message.
