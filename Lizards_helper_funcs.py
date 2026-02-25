5a7b79b5bb83a94cda4c7f63281242c252c163bf
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-02-25 09:12:44 -0700

    Added functions to file
    Added a function for input validation with various methods and a decorator for looping function execution.

--- Lizards_helper_funcs.py
+++ Lizards_helper_funcs.py
@@ -1,5 +1,44 @@
 #Contains useful helper functions for all sorts of applications
 
 #stupid proofed inputs function
+def stupid_proofed_inputs(message,method,*allowed_inputs):
+    while True:
+        user_data = ""
+        if method == "lower":
+            user_data = input(f"\n\n{message}").strip().lower()
+        elif method == "title":
+            user_data = input(f"\n\n{message}").strip().title()
+        elif method == "number":
+            while not user_data.isnumeric():
+                user_data = input(f"\n\n{message}").strip()
+                if not user_data.isnumeric():
+                    print("\n\nWrite a number")
+        elif method == "none":
+            user_data = input(f"\n\n{message}").strip()
+        else:
+            print("Programmer ERROR: used improper method")
+        if user_data in allowed_inputs:
+            return user_data
+        if "_" in allowed_inputs:
+            return user_data
+        else:
+            print("\n\nYou have inputed something in incorrectly please try again")
 
 #onlinator - when the onlinator decorator is applied to a function such as main, it recreates the while loop that keeps the code running until the user chooses to quit, I may make a second version for main which has a different message.
+
+#decorator for loops
+def decorator(func):
+    def looper(*args):
+        while True:
+            func(*args)
+            if func.__name__ == "main":
+                done = stupid_proofed_inputs("Are you done using this program? (y/n) ?","lower","y","n")
+            else:
+                done = stupid_proofed_inputs("Are you done using this tool? (y/n) ?","lower","y","n")
+            if done == "y":
+                break
+            elif done == "n":
+                print("continue")
+            else:
+                print("Please only type y for yes or n for no.")
+    return looper
