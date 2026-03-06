bcf8631ad3a97ed9a7c53e3694bf635dbf53a29b
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-03-06 09:42:45 -0700

    Code Works again!

--- Brett.py
+++ Brett.py
@@ -4,18 +4,18 @@ from Lizards_helper_funcs import *
 import hashlib
 from Gov.tic_tac_toe_remix import *
 from Gov.Gov import *
-import os
 
+def what():
+    with open("HighScoretracker-CP2-\\hi_file.csv", newline='\n') as file:
+        fieldnames = ["username", "password"]
+        reader = json.reader(file)
 #funtion to welcome user as "welcome"
 def welcome():
     while True:
-        os.system("cls")
         file = JSON_reader()
         #display welcome to the highscore tracker
         print("Welcome to the highscore tracker: ")
         #enter a number 1 to check if you have an account 2 to login 3 to create an account 4 to exit program
-        print("1 to check if you have an account\n2 to login\n3 to create an account\n4 to look at the leaderboard\n5 to exit program: ")
-        user_in = stupid_proofed_inputs("What do you want to do: ", "number", "1", "2", "3", "4","5")
         print("1 to check if you have an account\n2 to login\n3 to create an account\n4 to look at the leaderboard\n5 to exit program: ")
         user_in = stupid_proofed_inputs("What do you want to do: ", "number", "1", "2", "3", "4","5")
         #if user entered 1
@@ -34,12 +34,6 @@ def welcome():
             scores_list(csv_reader())
         elif user_in == "5":
             #break/leave program
-            print("Leaving program")
-            #show scores
-            scores_list(csv_reader())
-        elif user_in == "5":
-            #break/leave program
-            print("Leaving program")
             break
         #else
         else:
@@ -62,13 +56,36 @@ def enter_username(file):
             #call login
             login(file)
 
+#function to check_username()
+def check_username(file):
+    #while true
+    while True:
+        #ask user (y) to renter username or (n) to create an account
+        user = stupid_proofed_inputs("press (y) to renter username or (n) to create an account (r) to restart the login prosses (l) to leave: ", "lower", "y", "n" ,"r", "l")
+            #if user entered (y)
+        if user == "y":
+            #call enter_username funtion
+            enter_username(file)
+        #else if user entered n
+        elif user == "n":    
+            #call create()
+            #call login
+            login(file)
+        elif user == "l":
+            return
+        #else
+        else:            
+            #display enter a valid option
+            print("Enter valid option: ")
+            #continue
+            continue
+        
+
 #function for creating an account as "create()"
 def create(file):
     #while code is not false
     while True:
         #user enter username
-        print("Must make an account then login")
-        print("Must make an account then login")
         username = stupid_proofed_inputs("Enter a username for your account: ", "none", "_")
         if username not in file:
             #save username in json file
@@ -96,9 +113,7 @@ def login(file):
                 #call the game file
                 game_stats = play_game()
                 game_stats.insert(0,username)
-                game_stats.insert(0,username)
                 score_formats(csv_reader(),game_stats)
-                break
                 break
             else:
                 print("Password wrong")
@@ -113,4 +128,3 @@ def login(file):
 def check_account(file):
     for i in file.keys():
         print(f"usernames: {i}")
-
